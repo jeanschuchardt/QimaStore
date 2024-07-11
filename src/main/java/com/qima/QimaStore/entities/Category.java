02eeb250cbb22ac82f4450e2fca2640b638b6989
Jean Schuchardt <jeanschuchardt@hotmail.com>
2024-07-11 16:56:05 -0300

    create category class

---
+++ src/main/java/com/qima/QimaStore/entities/Category.java
@@ -0,0 +1,30 @@
+package com.qima.QimaStore.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Table(name = "categories")
+@Data
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> subcategories;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+
+}
