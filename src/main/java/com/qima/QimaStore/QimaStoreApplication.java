f8bcda24f0191e9d737afa918c5e1ba2c98d6138
Jean Schuchardt <jeanschuchardt@hotmail.com>
2024-07-11 16:39:47 -0300

    Project Start

---
+++ src/main/java/com/qima/QimaStore/QimaStoreApplication.java
@@ -0,0 +1,13 @@
+package com.qima.QimaStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QimaStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QimaStoreApplication.class, args);
+	}
+
+}
